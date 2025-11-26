1f1421d9153806e21aaa3b74575c1bef9de878ac
Matthijs Hulshof <m.hulshof03@student.alfa-college.nl>
2025-11-26 17:46:04 +0100

    Added a navbar Added "Openstaand" and "Afgerond" buttons with corresponding colors

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Werkbonnen</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 </head>
-<body>
-    <a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>button</a>
 
+<body>
+    <nav>
+        <div class="nav-wrapper">
+            <a href="/" class="brand-logo">Werkbonnen</a>
+            <ul class="right hide-on-med-and-down">
+                <li><a class="waves-effect waves-light btn indigo">Nieuwe Werkbon<i class="material-icons left">add</i></a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col s6 m6 center-align">
+                <a href="#" class="waves-effect waves-light btn-large amber lighten-2"><i class="material-icons left">lock_open</i>Openstaand</a>
+            </div>
+            <div class="col s6 m6 center-align">
+                <a href="#" class="waves-effect waves-light btn-large green lighten-1"><i class="material-icons left">lock</i>Afgerond</a>
+            </div>
+        </div>
+    </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 </html>
