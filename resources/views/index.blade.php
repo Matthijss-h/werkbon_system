ea0ea2aba1ed7a0ff5f5c557ca27260d4da845c1
Matthijs Hulshof <m.hulshof03@student.alfa-college.nl>
2025-11-26 22:55:59 +0100

    Added cards that show "Werkbonnen" Addded margin to the logo and the buttons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,23 +13,76 @@
 <body>
     <nav>
         <div class="nav-wrapper">
-            <a href="/" class="brand-logo">Werkbonnen</a>
+            <a href="/" class="brand-logo" style="margin-left: 15px;">Werkbonnen</a>
             <ul class="right hide-on-med-and-down">
-                <li><a class="waves-effect waves-light btn indigo">Nieuwe Werkbon<i class="material-icons left">add</i></a></li>
+                <li><a class="waves-effect waves-light btn indigo">Nieuwe Werkbon<i
+                            class="material-icons left">add</i></a></li>
             </ul>
         </div>
     </nav>
 
-    <div class="container">
+    <div class="container section">
         <div class="row">
             <div class="col s6 m6 center-align">
-                <a href="#" class="waves-effect waves-light btn-large amber lighten-2"><i class="material-icons left">lock_open</i>Openstaand</a>
+                <a href="#" class="waves-effect waves-light btn-large amber col s12 text-white">
+                    <i class="material-icons left">lock_open</i>Openstaand
+                </a>
             </div>
             <div class="col s6 m6 center-align">
-                <a href="#" class="waves-effect waves-light btn-large green lighten-1"><i class="material-icons left">lock</i>Afgerond</a>
+                <a href="#" class="waves-effect waves-light btn-large green lighten-1 col s12">
+                    <i class="material-icons left">lock</i>Afgerond
+                </a>
             </div>
         </div>
     </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col s10 m10 offset-s1 offset-m1">
+                <div class="card blue-grey darken-1">
+                    <div class="card-content white-text">
+                        <span class="card-title"><i class="material-icons left">person</i>Naam Medewerker</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Fusce dapibus fermentum nibh, ut tincidunt metus tincidunt eget.
+                            Vestibulum vehicula, augue vitae efficitur rhoncus, sem felis.</p>
+                    </div>
+                    <div class="card-action center">
+                        <a href="#" class="waves-effect waves-light btn blue">
+                            <i class="material-icons left">visibility</i>Bekijken
+                        </a>
+
+                        <a href="#" class="waves-effect waves-light btn green darken-1">
+                    <i class="material-icons left">check</i>Afronden
+                </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col s10 m10 offset-s1 offset-m1">
+                <div class="card blue-grey darken-1">
+                    <div class="card-content white-text">
+                        <span class="card-title"><i class="material-icons left">person</i>Naam Medewerker</span>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Fusce dapibus fermentum nibh, ut tincidunt metus tincidunt eget.
+                            Vestibulum vehicula, augue vitae efficitur rhoncus, sem felis.</p>
+                    </div>
+                    <div class="card-action center">
+                        <a href="#" class="waves-effect waves-light btn blue">
+                            <i class="material-icons left">visibility</i>Bekijken
+                        </a>
+
+                        <a href="#" class="waves-effect waves-light btn green darken-1">
+                    <i class="material-icons left">check</i>Afronden
+                </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
+
 </html>
