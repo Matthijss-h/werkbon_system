a63c9683a21a3f2c4d8edc75bb35597f865233b7
Matthijs Hulshof <m.hulshof03@student.alfa-college.nl>
2025-11-27 16:52:58 +0100

    Created Werkbon models, migrations, factories, and  controllers.

---
+++ app/Http/Controllers/WerkbonController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Werkbon;
+use Illuminate\Http\Request;
+
+class WerkbonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Werkbon $werkbon)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Werkbon $werkbon)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Werkbon $werkbon)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Werkbon $werkbon)
+    {
+        //
+    }
+}
---
+++ app/Models/WerkbonFoto.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WerkbonFoto extends Model
+{
+    /** @use HasFactory<\Database\Factories\WerkbonFotoFactory> */
+    use HasFactory;
+}
