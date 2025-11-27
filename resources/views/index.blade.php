6cb4f7ef8987f7e0ed60802beb455b6f0ffda070
Matthijs Hulshof <m.hulshof03@student.alfa-college.nl>
2025-11-27 14:24:33 +0100

    Added layout.blade.php for an easy reusable layout Changed positions of "Bekijken" and "Afronden" buttons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,41 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Werkbonnen</title>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-</head>
-
-<body>
-    <nav>
-        <div class="nav-wrapper">
-            <a href="/" class="brand-logo" style="margin-left: 15px;">Werkbonnen</a>
-            <ul class="right hide-on-med-and-down">
-                <li><a class="waves-effect waves-light btn indigo">Nieuwe Werkbon<i
-                            class="material-icons left">add</i></a></li>
-            </ul>
-        </div>
-    </nav>
-
-    <div class="container section">
-        <div class="row">
-            <div class="col s6 m6 center-align">
-                <a href="#" class="waves-effect waves-light btn-large amber col s12 text-white">
-                    <i class="material-icons left">lock_open</i>Openstaand
-                </a>
-            </div>
-            <div class="col s6 m6 center-align">
-                <a href="#" class="waves-effect waves-light btn-large green lighten-1 col s12">
-                    <i class="material-icons left">lock</i>Afgerond
-                </a>
-            </div>
-        </div>
-    </div>
-
+<x-layout>
     <div class="container">
         <div class="row">
             <div class="col s10 m10 offset-s1 offset-m1">
@@ -46,20 +9,25 @@
                             Fusce dapibus fermentum nibh, ut tincidunt metus tincidunt eget.
                             Vestibulum vehicula, augue vitae efficitur rhoncus, sem felis.</p>
                     </div>
-                    <div class="card-action center">
-                        <a href="#" class="waves-effect waves-light btn blue">
-                            <i class="material-icons left">visibility</i>Bekijken
-                        </a>
-
-                        <a href="#" class="waves-effect waves-light btn green darken-1">
-                    <i class="material-icons left">check</i>Afronden
-                </a>
+                    <div class="card-action">
+                        <div class="row" style="margin-bottom: 0;">
+                            <div class="col s6 center">
+                                <a href="#" class="waves-effect waves-light btn blue">
+                                    <i class="material-icons left">visibility</i>Bekijken
+                                </a>
+                            </div>
+                            <div class="col s6 center">
+                                <a href="#" class="waves-effect waves-light btn green darken-1">
+                                    <i class="material-icons left">check</i>Afronden
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="row">
+                <div class="row">
             <div class="col s10 m10 offset-s1 offset-m1">
                 <div class="card blue-grey darken-1">
                     <div class="card-content white-text">
@@ -68,21 +36,23 @@
                             Fusce dapibus fermentum nibh, ut tincidunt metus tincidunt eget.
                             Vestibulum vehicula, augue vitae efficitur rhoncus, sem felis.</p>
                     </div>
-                    <div class="card-action center">
-                        <a href="#" class="waves-effect waves-light btn blue">
-                            <i class="material-icons left">visibility</i>Bekijken
-                        </a>
-
-                        <a href="#" class="waves-effect waves-light btn green darken-1">
-                    <i class="material-icons left">check</i>Afronden
-                </a>
+                    <div class="card-action">
+                        <div class="row" style="margin-bottom: 0;">
+                            <div class="col s6 center">
+                                <a href="#" class="waves-effect waves-light btn blue">
+                                    <i class="material-icons left">visibility</i>Bekijken
+                                </a>
+                            </div>
+                            <div class="col s6 center">
+                                <a href="#" class="waves-effect waves-light btn green darken-1">
+                                    <i class="material-icons left">check</i>Afronden
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-
+        
     </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-</body>
-
-</html>
+</x-layout>
