851576a6455b906fae6d42fda165b6b4dfe53d78
Matthijs Hulshof <m.hulshof03@student.alfa-college.nl>
2025-12-07 12:05:36 +0100

    Add create workorder view and route

--- app/Http/Controllers/WorkorderController.php
+++ app/Http/Controllers/WorkorderController.php
@@ -22,7 +22,7 @@ class WorkorderController extends Controller
      */
     public function create()
     {
-        //
+        return view('workorders.create');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,8 @@ Route::get('/workorders/{status}', [WorkorderController::class, 'index'])
     ->where('status', 'open|closed')
     ->name('workorders.index');
 
+Route::get('/workorder/create', [WorkorderController::class, 'create'])
+    ->name('workorders.create');
+
 Route::get('/workorder/{workorder}', [WorkorderController::class, 'show'])
     ->name('workorders.show');
